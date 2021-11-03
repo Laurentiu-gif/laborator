9506defddeb7093463e450c6ff148e684e936b9c
Laurentiu-gif <laurentiu.andris@yahoo.com>
2021-11-03 21:29:30 +0200

    Tema 4

---
+++ Andris Laurentiu/L04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.table;
+
+namespace L04
+{
+    class Program
+    {
+        private static CloudTableClient tableClient;
+
+        private static CloudTable studentsTable;
+        static void Main(string[] args)
+        {
+            Task.Run(async () => {await Initialize(); })
+                .GetAwaiter()
+                .GetResult();
+        }
+
+        static async Task Initialize()
+        {
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=storagedatc;AccountKey=pHlt1hdQBsujMfclBd73xC1LkBLjUwG5oFQWXIoHvOE86aZleQJAcfHvSoTBlkySm+vNHTlrj4AkiP7/FSCbYg==;EndpointSuffix=core.windows.net";
+            var account = CloudStorageAccount.Parse(storageConnectionString);
+            tableClient = account.CreateCloudTableClient();
+
+            studentsTable = tableClient.GetTableReference("studenti");
+            await studentsTable.CreateIfNotExistsAsync();
+
+
+        }
+    }
+}
